7cc290f3c96c6b9ddb0f0e6a8fd22b7dcc494fe2
wyc1634773572 <69286742+wyc1634773572@users.noreply.github.com>
2022-09-14 10:57:20 +0800

    Update opcv_practice1.cc
    添加了对单个像素各通道的处理

--- opcv_practice1.cc
+++ opcv_practice1.cc
@@ -5,9 +5,19 @@
 using namespace std;
 using namespace cv;
 
+#define COLOR_DIV 100
+
 int main()
 {
     Mat img = imread("girl.jpg",1);
+    for (int i = 0;i < img.rows;i++) {
+        for (int j = 0;j < img.cols;) {
+            img.at<Vec3b>(i, j)[0] = img.at<Vec3b>(i, j)[0] / COLOR_DIV * COLOR_DIV + COLOR_DIV / 2;
+            img.at<Vec3b>(i, j)[1] = img.at<Vec3b>(i, j)[1] / COLOR_DIV * COLOR_DIV + COLOR_DIV / 2;
+            img.at<Vec3b>(i, j)[2] = img.at<Vec3b>(i, j)[2] / COLOR_DIV * COLOR_DIV + COLOR_DIV / 2;
+            j += 2;
+        }
+    }
     //namedWindow("opecv_win");
     imshow("opecv_win",img);
 
