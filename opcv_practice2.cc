e5538e82904092e674f9583f5e28fcfe5bc221d7
wyc1634773572 <69286742+wyc1634773572@users.noreply.github.com>
2022-09-30 17:35:32 +0800

    Update opcv_practice2.cc

--- opcv_practice2.cc
+++ opcv_practice2.cc
@@ -5,23 +5,42 @@
 using namespace std;
 using namespace cv;
 
-Mat g_srcim, g_dstboxim;
+Mat g_srcim, g_dstboxim, g_dstblurim, g_dstgaussim;
+static int default_val = 5;
 
 void OnBoxChange(int blurval, void* v) {
     boxFilter(g_srcim,g_dstboxim,-1,Size(blurval +1, blurval +1));
-    imshow("dst", g_dstboxim);
+    imshow("boxfilter", g_dstboxim);
+}
+
+void OnBlurChange(int blurval, void* v) {
+    blur(g_srcim, g_dstblurim, Size(blurval + 1, blurval + 1));
+    imshow("blur", g_dstblurim);
+}
+
+void OnGaussChange(int blurval, void* v) {
+    GaussianBlur(g_srcim, g_dstgaussim, Size(blurval*2 + 1, blurval*2 + 1),0,0);
+    imshow("gauss", g_dstgaussim);
 }
 
 int main()
 {
-    int default_val = 5;
-
     g_srcim = imread("C:\\Users\\wangyicheng\\Desktop\\restudy\\girl_480.jpg",1);
     g_dstboxim = g_srcim.clone();
-    imshow("srcimage", g_srcim);
-    namedWindow("dst");             //Must creat new window before createTrackbar
+    g_dstblurim = g_srcim.clone();
+    g_dstgaussim = g_srcim.clone();
 
-    createTrackbar("内核size", "dst",&default_val,10, OnBoxChange);
+    imshow("srcimage", g_srcim);
+
+    namedWindow("boxfilter");             //Must creat new window before createTrackbar
+    createTrackbar("内核size", "boxfilter",&default_val,10, OnBoxChange);
+
+    namedWindow("blur");
+    createTrackbar("内核size", "blur", &default_val, 10, OnBlurChange);
+
+    namedWindow("gauss");
+    createTrackbar("内核size", "gauss", &default_val, 10, OnGaussChange);
     
     waitKey(0);
 }
+
