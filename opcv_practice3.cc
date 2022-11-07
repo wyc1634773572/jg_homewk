27d9f6de6169c4193a09c9b35704874c36877c15
wyc1634773572 <69286742+wyc1634773572@users.noreply.github.com>
2022-11-07 14:07:24 +0800

    Create opcv_practice3.cc

---
+++ opcv_practice3.cc
@@ -0,0 +1,38 @@
+#include<opencv2/core/core.hpp>
+#include<opencv2/highgui/highgui.hpp>
+#include<opencv2/imgproc.hpp>
+#include <iostream>
+using namespace std;
+using namespace cv;
+
+
+int main()
+{
+	Mat girlimg = imread("C:\\Users\\wangyicheng\\Desktop\\restudy\\girl_480.jpg", 1);
+	Mat girlimg2 = imread("C:\\Users\\wangyicheng\\Desktop\\restudy\\girl_480.jpg",1);
+	Mat girlimg3 = imread("C:\\Users\\wangyicheng\\Desktop\\restudy\\girl_480.jpg", 1);
+	Mat sceneimg = imread("C:\\Users\\wangyicheng\\Desktop\\restudy\\scene.jpg");
+	Mat lightimg = imread("C:\\Users\\wangyicheng\\Desktop\\restudy\\light.jpg", 1);
+	Mat mask = imread("C:\\Users\\wangyicheng\\Desktop\\restudy\\light.jpg", 0);
+	vector<Mat> channel;
+
+	Mat ROI = girlimg(Rect(128,128, lightimg.cols, lightimg.rows));
+	lightimg.copyTo(ROI, lightimg);
+
+	split(girlimg2, channel);
+
+	cv::resize(sceneimg, sceneimg, Size(480, 480));
+	addWeighted(girlimg3, 0.6, sceneimg, 0.4, 0.0, girlimg3);
+	
+
+	imshow("ROI", girlimg);
+	imshow("split1", channel[0]);
+	imshow("split2", channel[1]);
+	imshow("split3", channel[2]);
+	imshow("addweight", girlimg3);
+	waitKey(0);
+	imwrite("C:\\Users\\wangyicheng\\Desktop\\restudy\\ROI.jpg", girlimg);
+	imwrite("C:\\Users\\wangyicheng\\Desktop\\restudy\\split.jpg", channel[0]);
+	imwrite("C:\\Users\\wangyicheng\\Desktop\\restudy\\addWeighted.jpg", girlimg3);
+    return 0;
+}
